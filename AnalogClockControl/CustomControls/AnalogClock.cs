d5cf69a88e3d1ec90beb056a0a53968aa2ec22a5
Jeremy Sandlin <jeremy@kenyonnoble.com>
2021-10-19 14:15:27 -0600

    added context menu and more

--- AnalogClockControl/CustomControls/AnalogClock.cs
+++ AnalogClockControl/CustomControls/AnalogClock.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Drawing;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,19 +15,38 @@ namespace AnalogClockControl.CustomControls
     {
         private readonly DispatcherTimer dispTimer = new DispatcherTimer(DispatcherPriority.Send);
 
-        private Line hourHand;
-        private Line minuteHand;
-        private Line secondHand;
-        private TextBlock digital;
-        private TextBlock digital2;
-        private Grid ticks;
-        private Ellipse border;
-        private Ellipse centerRing;
+        private ArrayList shapeParts = new ArrayList();
+        private ArrayList textBlockParts = new ArrayList();
+        private Line        hourHand;
+        private Line        minuteHand;
+        private Line        secondHand;
+        private Ellipse     border;
+        private Ellipse     centerRing;
+        private TextBlock   digital;
+        private TextBlock   digital2;
+        private Grid        ticks;
+
+        private ContextMenu cm = new ContextMenu();
 
 
-        static AnalogClock()
+        public AnalogClock()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(AnalogClock), new FrameworkPropertyMetadata(typeof(AnalogClock)));
+
+            this.ContextMenu = cm;
+            var mi = new MenuItem();
+            var mi2 = new MenuItem();
+            var mi3 = new MenuItem();
+            cm.Items.Add(mi2);
+            cm.Items.Add(mi3);
+            cm.Items.Add(mi);
+            mi.Header = "Close";
+            mi.Icon = MakeIcon("pack://application:,,,/Resources/ChromeClose_16x.png", 16);
+            mi.Click += (s, e) => Application.Current.Shutdown();
+            mi2.Header = "Dark Mode";
+            mi3.Header = "Light Mode";
+            mi2.Click += (s, e) => SetThemeColor(Colors.Black);
+            mi3.Click += (s, e) => SetThemeColor(Colors.White);
         }
 
         public override void OnApplyTemplate()
@@ -39,6 +60,14 @@ namespace AnalogClockControl.CustomControls
             border = Template.FindName("PART_Border", this) as Ellipse;
             centerRing = Template.FindName("PART_Center2", this) as Ellipse;
 
+            shapeParts.Add(hourHand);
+            shapeParts.Add(minuteHand);
+            shapeParts.Add(secondHand);
+            shapeParts.Add(border);
+            shapeParts.Add(centerRing);
+            textBlockParts.Add(digital);
+            textBlockParts.Add(digital2);
+
             dispTimer.Interval = TimeSpan.FromMilliseconds(1000);
             dispTimer.Tick += new EventHandler(UpdateClocks);
             dispTimer.IsEnabled = true;
@@ -48,20 +77,28 @@ namespace AnalogClockControl.CustomControls
             base.OnApplyTemplate();
         }
 
+        private void SetThemeColor()
+        {
+            var rnd = new Random();
+            var bytes = new byte[3];
+            rnd.NextBytes(bytes);
+            var rndColor = Color.FromRgb(bytes[0], bytes[1], bytes[2]);
+            SetThemeColor(rndColor);
+        }
+
         private void SetThemeColor(Color color)
         {
             var brush = new SolidColorBrush(color);
 
-            border.Stroke = brush;
-            hourHand.Stroke = brush;
-            minuteHand.Stroke = brush;
-            digital.Foreground = brush;
-            digital2.Foreground = brush;
-            centerRing.Stroke = brush;
-
-            var kids = ticks.Children;
-            foreach (Line item in kids)
+            foreach (Shape item in shapeParts)
                 item.Stroke = brush;
+
+            foreach (Line item in ticks.Children)
+                item.Stroke = brush;
+
+            foreach (TextBlock item in textBlockParts)
+                item.Foreground = brush;
+
         }
 
         private void UpdateClocks(object sender, EventArgs e)
@@ -72,6 +109,20 @@ namespace AnalogClockControl.CustomControls
 
             digital.Text = DateTime.Now.ToString("hh:mm:ss");
             digital2.Text = DateTime.Now.ToString("tt");
+
+            //SetThemeColor();
+        }
+
+        private Image MakeIcon(string path, int pixelWidth)
+        {
+            System.Windows.Media.Imaging.BitmapImage srcBmp = new System.Windows.Media.Imaging.BitmapImage();
+            srcBmp.BeginInit();
+            srcBmp.UriSource = new Uri(path);
+            srcBmp.DecodePixelWidth = pixelWidth;
+            srcBmp.EndInit();
+            Image icon = new Image();
+            icon.Source = srcBmp;
+            return icon;
         }
     }
 }
